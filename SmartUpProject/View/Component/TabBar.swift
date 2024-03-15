2762e8d58ea25fdac3bda8cd7d49789354fc9992
MTFL1 <m.ecomplus1@gmail.com>
2024-03-15 14:12:40 +0100

    add TabBar

--- SmartUpProject/View/Component/TabBar.swift
+++ SmartUpProject/View/Component/TabBar.swift
@@ -9,7 +9,30 @@ import SwiftUI
 
 struct TabBar: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        TabView {
+
+        Projet()
+            .tabItem {
+                Label("Projet", systemImage: "folder.badge.plus")
+            }
+        
+        Note()
+            .tabItem {
+                Label("Note", systemImage: "list.bullet.clipboard")
+            }
+        
+        CarteView()
+            .tabItem {
+                Label("Map", systemImage: "map")
+            }
+        
+            HistoriqueView(historique: Historique(name: "", icon: "", date: ""))
+            .tabItem {
+                Label("Historique", systemImage: "clock.arrow.circlepath")
+            }
+    }
+        .tint(Color(red: 0.613, green: 0.51, blue: 0.745))
     }
 }
 
